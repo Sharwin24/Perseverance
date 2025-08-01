2b09de9087155c7a43b3c98af581675b0273215a
Sharwin24 <sharwin24@gmail.com>
2025-08-01 18:13:39 -0500

    kalman_filter.cpp

---
+++ state_estimation/src/kalman_filter.cpp
@@ -0,0 +1,9 @@
+#include "kalman_filter.hpp"
+
+RobotState KalmanFilter::predictDynamicModel(const sensor_msgs::msg::Imu imu, const double timestamp) {
+
+}
+
+RobotState KalmanFilter::predictKinematicModel(const double left_vel, const double right_vel, const double timestamp) {
+
+}
