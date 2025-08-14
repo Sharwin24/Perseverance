ec65539ad279d73c20372e93e70221c4a8e51be5
Sharwin24 <sharwin24@gmail.com>
2025-08-14 01:05:51 -0500

    Implemented all prediction models

--- state_estimation/include/state_estimation/kalman_filter.hpp
+++ state_estimation/include/state_estimation/kalman_filter.hpp
@@ -3,6 +3,18 @@
 
 #include <eigen3/Eigen/Dense>
 #include <sensor_msgs/msg/imu.hpp>
+#include <unordered_map>
+#include <string>
+
+// MECANUM wheel names (4)
+constexpr std::array<const char*, 4> MECANUM_WHEEL_NAMES = {
+  "front_left", "front_right", "rear_left", "rear_right"
+};
+
+// ROCKER_BOGIE wheel names (6)
+constexpr std::array<const char*, 6> ROCKER_BOGIE_WHEEL_NAMES = {
+  "front_left", "front_right", "middle_left", "middle_right", "rear_left", "rear_right"
+};
 
 struct RobotState {
   double x = 0.0; // X Position [m]
@@ -22,6 +34,13 @@ struct RobotState {
     this->normalizeTheta();
   }
 
+  /**
+   * @brief Convert the RobotState to an Eigen vector.
+   *
+   * @note Not an eigen-vector, a Vector type from the Eigen Library
+   *
+   * @return Eigen::Vector<double, 6> The 6x1 state vector [x, y, theta, vx, vy, omega]
+   */
   Eigen::Vector<double, 6> vec() const {
     return Eigen::Vector<double, 6>(x, y, theta, vx, vy, omega);
   }
@@ -36,9 +55,13 @@ struct RobotState {
 };
 
 struct Covariance {
+  // The 6x6 state covariance matrix (P), describing the uncertainty of the state estimate
   Eigen::Matrix<double, 6, 6> stateCovariance = Eigen::Matrix<double, 6, 6>::Identity();
+  // The 6x6 process noise covariance matrix (Q), describing the uncertainty in the process model
   Eigen::Matrix<double, 6, 6> processNoiseCovariance = Eigen::Matrix<double, 6, 6>::Identity();
+  // The 3x3 odometry measurement noise covariance matrix (R_odom), describing the uncertainty in the odometry measurements
   Eigen::Matrix<double, 3, 3> odomMeasurementNoiseCovariance = Eigen::Matrix<double, 3, 3>::Identity();
+  // The 1x1 IMU measurement noise covariance matrix (R_imu), describing the uncertainty in the IMU measurements
   Eigen::Matrix<double, 1, 1> imuMeasurementNoiseCovariance = Eigen::Matrix<double, 1, 1>::Identity();
 
   Covariance() = default;
@@ -110,10 +133,19 @@ struct Covariance {
   }
 };
 
+enum class KinematicModel {
+  // 2 wheel differential drive
+  DIFF_DRIVE,
+  // 4 wheel omni-directional mecanum drive
+  MECANUM,
+  // 6 wheel rocker-bogie drive with the 4 corner wheels capable of independent steering
+  ROCKER_BOGIE
+};
+
 enum class PredictionModel {
   // Uses the dynamics of the physical system (acceleration) to predict the state
   DYNAMIC,
-  // Uses the kinematic model of the diff-drive drivebase to predict the state
+  // Uses the kinematic model of the drivebase to predict the state
   KINEMATIC
 };
 
@@ -121,11 +153,15 @@ struct SystemModel {
   Eigen::Matrix<double, 6, 6> stateTransitionMatrix = Eigen::Matrix<double, 6, 6>::Zero();
   Eigen::Matrix<double, 6, 2> controlInputModel = Eigen::Matrix<double, 6, 2>::Zero();
   PredictionModel predModel = PredictionModel::DYNAMIC;
+  KinematicModel kinematicModel = KinematicModel::DIFF_DRIVE;
 
   SystemModel() = default;
-  SystemModel(PredictionModel predModel) : predModel(predModel) {};
+  SystemModel(PredictionModel predModel, KinematicModel kinematicModel) : predModel(predModel), kinematicModel(kinematicModel) {}
 
-  Eigen::Matrix<double, 6, 6> getStateTransitionMatrix(const double dt, const double V = 0, const double W = 0, const double theta = 0) {
+  Eigen::Matrix<double, 6, 6> getStateTransitionMatrix(
+    const double dt,
+    const double V = 0, const double W = 0, const double theta = 0,
+    const double vX = 0, const double vY = 0) {
     switch (predModel) {
     case PredictionModel::DYNAMIC: {
       stateTransitionMatrix(0, 0) = 1.0;
@@ -137,16 +173,39 @@ struct SystemModel {
       stateTransitionMatrix(0, 3) = dt; // dX/dVx
       stateTransitionMatrix(1, 4) = dt; // dY/dVy
       stateTransitionMatrix(2, 5) = dt; // dTheta/dOmega
+      break;
     }
     case PredictionModel::KINEMATIC: {
-      stateTransitionMatrix(0, 0) = 1.0; // dX/dx
-      stateTransitionMatrix(0, 2) = -1.0 * V * std::sin(theta) * dt; // dX/dtheta
-      stateTransitionMatrix(1, 1) = 1.0; // dY/dy
-      stateTransitionMatrix(1, 2) = V * std::cos(theta) * dt; // dY/dtheta
-      stateTransitionMatrix(2, 2) = 1.0; // dTheta/dtheta
-      stateTransitionMatrix(3, 2) = -1.0 * V * std::sin(theta) * (W * dt); // dVx/dtheta
-      stateTransitionMatrix(4, 2) = V * std::cos(theta) * (W * dt); // dVy/dtheta
-
+      switch (kinematicModel) {
+      case KinematicModel::DIFF_DRIVE: {
+        stateTransitionMatrix(0, 0) = 1.0; // dX/dx
+        stateTransitionMatrix(0, 2) = -1.0 * V * std::sin(theta) * dt; // dX/dtheta
+        stateTransitionMatrix(1, 1) = 1.0; // dY/dy
+        stateTransitionMatrix(1, 2) = V * std::cos(theta) * dt; // dY/dtheta
+        stateTransitionMatrix(2, 2) = 1.0; // dTheta/dtheta
+        stateTransitionMatrix(3, 2) = -1.0 * V * std::sin(theta) * (W * dt); // dVx/dtheta
+        stateTransitionMatrix(4, 2) = V * std::cos(theta) * (W * dt); // dVy/dtheta
+        break;
+      }
+      case KinematicModel::MECANUM: {
+        stateTransitionMatrix(0, 2) = (-vX * std::sin(theta) - vY * std::cos(theta)) * dt; // dX/dtheta
+        stateTransitionMatrix(1, 2) = (vX * std::cos(theta) - vY * std::sin(theta)) * dt; // dY/dtheta
+        stateTransitionMatrix(0, 3) = dt; // dX/dVx
+        stateTransitionMatrix(1, 4) = dt; // dY/dVy
+        stateTransitionMatrix(2, 5) = dt; // dTheta/dOmega
+        break;
+      }
+      case KinematicModel::ROCKER_BOGIE: {
+        stateTransitionMatrix(0, 2) = (-vX * std::sin(theta) - vY * std::cos(theta)) * dt; // dX/dtheta
+        stateTransitionMatrix(1, 2) = (vX * std::cos(theta) - vY * std::sin(theta)) * dt; // dY/dtheta
+        stateTransitionMatrix(3, 2) = -vX * std::sin(theta) - vY * std::cos(theta); // dVx/dtheta
+        stateTransitionMatrix(4, 2) = vX * std::cos(theta) - vY * std::sin(theta); // dVy/dtheta
+        break;
+      }
+      default:
+        throw std::runtime_error("Unknown kinematic model type");
+      }
+      break;
     }
     }
     return stateTransitionMatrix;
@@ -174,16 +233,48 @@ struct MeasurementModel {
   }
 };
 
+
+
+struct KinematicModelInput {
+  // The velocity [rad/s] of the left wheel (DIFF_DRIVE)
+  double leftVelocity;
+  // The velocity [rad/s] of the right wheel (DIFF_DRIVE)
+  double rightVelocity;
+  // The velocities [rad/s] of the wheels (MECANUM, ROCKER_BOGIE)
+  // MECANUM keys: {"front_left", "front_right", "rear_left", "rear_right"}
+  // ROCKER_BOGIE keys: {"front_left", "front_right", "middle_left", "middle_right", "rear_left", "rear_right"}
+  std::unordered_map<std::string, double> wheelVelocities;
+
+  // The steering angles [rad] of the steerable wheels (ROCKER_BOGIE only)
+  // ROCKER_BOGIE keys: {"front_left", "front_right", "middle_left", "middle_right", "rear_left", "rear_right"}
+  std::unordered_map<std::string, double> wheelSteeringAngles;
+};
+
+struct RobotConstants {
+  // Wheel to Wheel distance along the Robot Y-axis [m]
+  double wheelBase;
+  // Wheel radius [m]
+  double wheelRadius;
+  // The back wheel to front wheel distance along the Robot X-axis [m]
+  double trackWidth;
+  // Map from wheel name to location
+  // MECANUM keys: {"front_left", "front_right", "rear_left", "rear_right"}
+  // ROCKER_BOGIE keys: {"front_left", "front_right", "middle_left", "middle_right", "rear_left", "rear_right"}
+  std::unordered_map<std::string, Eigen::Vector2d> wheelLocations;
+};
+
+
+
 class KalmanFilter {
 public:
   KalmanFilter() = default;
-  KalmanFilter(PredictionModel predModel, Covariance cov, RobotState initialState)
-    : predictionModel(predModel), systemModel(SystemModel(predModel)), covariance(cov), currentState(initialState) {
-  }
+  KalmanFilter(RobotConstants robotConstants, PredictionModel predModel, KinematicModel kinematicModel, Covariance cov, RobotState initialState)
+    : robotConstants(robotConstants), predictionModel(predModel), kinematicModel(kinematicModel),
+    systemModel(SystemModel(predModel, kinematicModel)), covariance(cov), currentState(initialState) {}
 
-  RobotState predictDynamicModel(const sensor_msgs::msg::Imu imu, const double timestamp);
+  RobotState predictDynamicModel(const sensor_msgs::msg::Imu imu, const long timestamp);
 
-  RobotState predictKinematicModel(const double left_vel, const double right_vel, const double timestamp);
+  RobotState predictKinematicModel(const KinematicModelInput& kinematicParams, const long timestamp);
 
   Eigen::Matrix<double, 3, 6> odomMeasurementModel() const { return this->measurementModel.odom; }
   Eigen::Matrix<double, 1, 6> imuMeasurementModel() const { return this->measurementModel.imu; }
@@ -199,20 +290,112 @@ public:
   }
 
 private:
+  // Robot constants (e.g., wheel base, wheel radius, track width)
+  RobotConstants robotConstants;
   // The type of prediction model to use for this filter
   PredictionModel predictionModel;
-
+  // The type of kinematic model to use for this filter
+  KinematicModel kinematicModel;
   // The state transition matrix F and the control input matrix B
   SystemModel systemModel;
-
   // Measurement models for the IMU and Odometry sensors
   MeasurementModel measurementModel;
-
   // Covariances relevant for both the system states and the sensors
   Covariance covariance;
-
   // The current robot's state estimation
   RobotState currentState;
+  // The timestamp of the previous prediction [s]
+  long previousPredictionTimeStamp;
+
+  Eigen::Matrix<double, 3, 4> mecanumForwardKinematics() const {
+    // Calculate the forward kinematics matrix for a mecanum drive robot
+    const double R = this->robotConstants.wheelRadius;
+    const double L = this->robotConstants.trackWidth / 2.0;
+    const double W = this->robotConstants.wheelBase / 2.0;
+    return (R / 4) * Eigen::Matrix<double, 3, 4>{
+      {-1 / (L + W), 1 / (L + W), 1 / (L + W), -1 / (L + W)},
+      {1, 1, 1, 1},
+      {-1, 1, -1, 1}
+    };
+  }
+
+  Eigen::Vector4d mecanumInverseKinematics(const double vX, const double vY, const double omega) const {
+    // Calculate wheel velocities from robot body velocities
+    const double R = this->robotConstants.wheelRadius;
+    const double L = this->robotConstants.trackWidth / 2.0;
+    const double W = this->robotConstants.wheelBase / 2.0;
+
+    return (1 / R) * Eigen::Vector4d{
+      vY - vX - (L + W) * omega, // Front left wheel
+      vY + vX + (L + W) * omega, // Front right wheel
+      vY - vX + (L + W) * omega, // Rear left wheel
+      vY + vX - (L + W) * omega  // Rear right wheel
+    };
+  }
+
+  Eigen::Vector3d rockerBogieForwardKinematics(
+    const std::unordered_map<std::string, double>& wheelVelocities,
+    const std::unordered_map<std::string, double>& wheelSteeringAngles) const {
+    // Calculate the forward kinematics for a rocker-bogie drive robot
+    // wheelVelocities: [front left, front right, middle left, middle right, rear left, rear right]
+    // wheelSteeringAngles: [front left, front right, rear left, rear right]
+    for (const auto& k : ROCKER_BOGIE_WHEEL_NAMES) {
+      if (wheelVelocities.find(k) == wheelVelocities.end()) {
+        throw std::invalid_argument(std::string("Missing wheel velocity key for ROCKER_BOGIE: ") + k);
+      }
+      if (wheelSteeringAngles.find(k) == wheelSteeringAngles.end()) {
+        throw std::invalid_argument(std::string("Missing wheel steering key for ROCKER_BOGIE: ") + k);
+      }
+    }
+
+    // Initialize Jacobian matrices
+    Eigen::Matrix<double, 12, 3> A = Eigen::Matrix<double, 12, 3>::Zero();
+    Eigen::Matrix<double, 12, 1> B = Eigen::Matrix<double, 12, 1>::Zero();
+    // Initialize row index and body velocity
+    unsigned int row = 0;
+    // Iterate over wheel names
+    for (const auto& wheelName : ROCKER_BOGIE_WHEEL_NAMES) {
+      // Get the wheel position, velocity, and steering angles
+      const Eigen::Vector2d& wheelPos = this->robotConstants.wheelLocations.at(wheelName);
+      const double wheelVel = wheelVelocities.at(wheelName);
+      const double wheelSteer = wheelSteeringAngles.at(wheelName);
+
+      // Populate A matrix for current wheel
+      // v_ix = v_bx - omega_b * y_i
+      A(row, 0) = 1;
+      A(row, 1) = 0;
+      A(row, 2) = -wheelPos.y();
+      // v_iy = v_by + omega_b * x_i
+      A(row + 1, 0) = 0;
+      A(row + 1, 1) = 1;
+      A(row + 1, 2) = wheelPos.x();
+
+      // Populate B matrix for current wheel
+      B(row, 0) = wheelVel * std::cos(wheelSteer);
+      B(row + 1, 0) = wheelVel * std::sin(wheelSteer);
+
+      // Move to the next wheel (2 rows per wheel)
+      row += 2;
+    }
+    Eigen::Vector3d twist; // [vx, vy, omega]
+    // QR Least Squares
+    auto qr = A.colPivHouseholderQr();
+    qr.setThreshold(1e-10);
+    if (qr.rank() == 3) {
+      twist = qr.solve(B);
+    }
+    else {
+      // Fallback: Complete Orthogonal Decomposition (min-norm)
+      twist = A.completeOrthogonalDecomposition().solve(B);
+    }
+    return twist;
+  }
+
+  RobotState predictDiffDriveKinematicModel(const double leftVelocity, const double rightVelocity, const long timestamp);
+
+  RobotState predictMecanumKinematicModel(const std::unordered_map<std::string, double>& wheelVelocities, const long timestamp);
+
+  RobotState predictRockerBogieKinematicModel(const std::unordered_map<std::string, double>& wheelVelocities, const std::unordered_map<std::string, double>& wheelSteeringAngles, const long timestamp);
 };
 
 #endif // !KALMAN_FILTER_HPP
--- state_estimation/src/kalman_filter.cpp
+++ state_estimation/src/kalman_filter.cpp
@@ -1,9 +1,200 @@
 #include "kalman_filter.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <array>
 
-RobotState KalmanFilter::predictDynamicModel(const sensor_msgs::msg::Imu imu, const double timestamp) {
+RobotState KalmanFilter::predictDynamicModel(const sensor_msgs::msg::Imu imu, const long timestamp) {
+  const long imuTimestamp = imu.header.stamp.sec + imu.header.stamp.nanosec / 1e9;
+  const long dt = imuTimestamp - this->previousPredictionTimeStamp;
+  if (dt <= 0) {
+    RCLCPP_WARN(rclcpp::get_logger("state_estimator"), "IMU message has non-positive timestamp difference. Skipping prediction");
+    return this->currentState; // No update if the timestamp is not valid
+  }
 
+  // Convert robot-frame acceleration to global-frame acceleration
+  const double theta = this->currentState.theta;
+  const double globalAccX = imu.linear_acceleration.x * std::cos(theta) - imu.linear_acceleration.y * std::sin(theta);
+  const double globalAccY = imu.linear_acceleration.x * std::sin(theta) + imu.linear_acceleration.y * std::cos(theta);
+
+  // Acquire State Transition matrix (F) and Control Input Model (B)
+  const auto F = this->systemModel.getStateTransitionMatrix(dt);
+  const auto B = this->systemModel.getControlInputModel(dt);
+
+  // Establish Control Vector (U)
+  const auto U = Eigen::Vector2d(globalAccX, globalAccY);
+
+  // Predict the next state: x_pred = F * x + B * u
+  Eigen::Vector<double, 6> predictedStateVector = F * this->currentState.vec() + B * U;
+  assert(predictedStateVector.size() == 6 && "State vector must have 6 elements");
+  RobotState predictedState(predictedStateVector);
+
+  // Update the state covariance with the process noise and state transition matrix
+  const auto P = this->covariance.stateCovariance;
+  const auto Q = this->covariance.processNoiseCovariance;
+  this->covariance.stateCovariance = F * P * F.transpose() + Q;
+
+  // Update the previous prediction timestamp
+  this->previousPredictionTimeStamp = imuTimestamp;
+
+  // Return the newly predicted state
+  return predictedState;
 }
 
-RobotState KalmanFilter::predictKinematicModel(const double left_vel, const double right_vel, const double timestamp) {
+RobotState KalmanFilter::predictKinematicModel(const KinematicModelInput& kinematicParams, const long timestamp) {
+  switch (this->kinematicModel) {
+  case KinematicModel::DIFF_DRIVE:
+    return this->predictDiffDriveKinematicModel(kinematicParams.leftVelocity, kinematicParams.rightVelocity, timestamp);
+  case KinematicModel::MECANUM:
+    if (kinematicParams.wheelVelocities.size() != MECANUM_WHEEL_NAMES.size()) {
+      throw std::invalid_argument(
+        "Invalid wheel velocities vector size for MECANUM model: "
+        + std::to_string(kinematicParams.wheelVelocities.size())
+        + " (expected 4: {front_left, front_right, rear_left, rear_right}).");
+    }
+    return this->predictMecanumKinematicModel(kinematicParams.wheelVelocities, timestamp);
+  case KinematicModel::ROCKER_BOGIE:
+    if (kinematicParams.wheelVelocities.size() != ROCKER_BOGIE_WHEEL_NAMES.size() ||
+      kinematicParams.wheelSteeringAngles.size() != ROCKER_BOGIE_WHEEL_NAMES.size()) {
+      throw std::invalid_argument(
+        "Invalid wheel velocities or steering angles map size for ROCKER_BOGIE model: "
+        + std::to_string(kinematicParams.wheelVelocities.size()) + " (expected 6: {front_left, front_right, middle_left, middle_right, rear_left, rear_right}) and "
+        + std::to_string(kinematicParams.wheelSteeringAngles.size()) + " (expected 6: {front_left, front_right, middle_left, middle_right, rear_left, rear_right}).");
+    }
+    return this->predictRockerBogieKinematicModel(kinematicParams.wheelVelocities, kinematicParams.wheelSteeringAngles, timestamp);
+  default:
+    throw std::runtime_error("Unknown kinematic model type.");
+  }
+}
 
+
+RobotState KalmanFilter::predictDiffDriveKinematicModel(const double leftVelocity, const double rightVelocity, const long timestamp) {
+  const double dt = timestamp - this->previousPredictionTimeStamp;
+  if (dt <= 0) {
+    RCLCPP_WARN(rclcpp::get_logger("state_estimator"), "Kinematic model prediction has non-positive timestamp difference. Skipping prediction");
+    return this->currentState; // No update if the timestamp is not valid
+  }
+
+  // Calculate kinematic inputs V (forward) and W (angular)
+  const auto V = (leftVelocity + rightVelocity) / 2.0; // Average velocity
+  const auto W = (rightVelocity - leftVelocity) / this->robotConstants.wheelBase; // Angular velocity
+
+  // Predict new state with non-linear state transition function
+  const auto xOld = this->currentState.x;
+  const auto yOld = this->currentState.y;
+  const auto thetaOld = this->currentState.theta;
+
+  // Update state using kinematic equations
+  const double xNew = xOld + V * std::cos(thetaOld) * dt;
+  const double yNew = yOld + V * std::sin(thetaOld) * dt;
+  const double thetaNew = thetaOld + W * dt;
+  const double vXNew = V * std::cos(thetaNew);
+  const double vYNew = V * std::sin(thetaNew);
+  RobotState predictedState(xNew, yNew, thetaNew, vXNew, vYNew, W);
+
+  // Calculate the Jacobian (F)
+  const auto F = this->systemModel.getStateTransitionMatrix(dt, V, W, thetaOld);
+
+  // Update the state covariance with the process noise and state transition matrix
+  const auto P = this->covariance.stateCovariance;
+  const auto Q = this->covariance.processNoiseCovariance;
+  this->covariance.stateCovariance = F * P * F.transpose() + Q;
+
+  // Update the previous prediction timestamp
+  this->previousPredictionTimeStamp = timestamp;
+
+  // Return the predicted state
+  return predictedState;
+}
+
+RobotState KalmanFilter::predictMecanumKinematicModel(const std::unordered_map<std::string, double>& wheelVelocities, const long timestamp) {
+  const double dt = timestamp - this->previousPredictionTimeStamp;
+  if (dt <= 0) {
+    RCLCPP_WARN(rclcpp::get_logger("state_estimator"), "Kinematic model prediction has non-positive timestamp difference. Skipping prediction");
+    return this->currentState; // No update if the timestamp is not valid
+  }
+
+  // Expected keys: front_left, front_right, rear_left, rear_right
+  for (const auto& k : MECANUM_WHEEL_NAMES) {
+    if (wheelVelocities.find(k) == wheelVelocities.end()) {
+      throw std::invalid_argument(std::string("Missing wheel velocity key for MECANUM: ") + k);
+    }
+  }
+
+  // Calculate the robot body twist
+  const Eigen::Vector4d wheelVelocitiesVector(
+    wheelVelocities.at("front_left"),
+    wheelVelocities.at("front_right"),
+    wheelVelocities.at("rear_left"),
+    wheelVelocities.at("rear_right")
+  );
+  const Eigen::Vector3d bodyTwist = this->mecanumForwardKinematics() * wheelVelocitiesVector;
+  // Convert to global frame
+  const double thetaOld = this->currentState.theta;
+  const double globalVX = bodyTwist(0) * std::cos(thetaOld) - bodyTwist(1) * std::sin(thetaOld);
+  const double globalVY = bodyTwist(0) * std::sin(thetaOld) + bodyTwist(1) * std::cos(thetaOld);
+  const double globalOmega = bodyTwist(2);
+  // Predict new state
+  const double xNew = this->currentState.x + globalVX * dt;
+  const double yNew = this->currentState.y + globalVY * dt;
+  const double thetaNew = this->currentState.theta + globalOmega * dt;
+  RobotState predictedState(xNew, yNew, thetaNew, globalVX, globalVY, globalOmega);
+
+  // Calculate the Jacobian (F)
+  const auto F = this->systemModel.getStateTransitionMatrix(dt, 0, 0, thetaOld, globalVX, globalVY);
+
+  // Update the state covariance with the process noise and state transition matrix
+  const auto P = this->covariance.stateCovariance;
+  const auto Q = this->covariance.processNoiseCovariance;
+  this->covariance.stateCovariance = F * P * F.transpose() + Q;
+
+  // Update the previous prediction timestamp
+  this->previousPredictionTimeStamp = timestamp;
+
+  // Return the predicted state
+  return predictedState;
+}
+
+RobotState KalmanFilter::predictRockerBogieKinematicModel(const std::unordered_map<std::string, double>& wheelVelocities, const std::unordered_map<std::string, double>& wheelSteeringAngles, const long timestamp) {
+  const double dt = timestamp - this->previousPredictionTimeStamp;
+  if (dt <= 0) {
+    RCLCPP_WARN(rclcpp::get_logger("state_estimator"), "Kinematic model prediction has non-positive timestamp difference. Skipping prediction");
+    return this->currentState; // No update if the timestamp is not valid
+  }
+
+  // Expected velocity keys (6) and steering keys (6)
+  for (const auto& k : ROCKER_BOGIE_WHEEL_NAMES) {
+    if (wheelVelocities.find(k) == wheelVelocities.end()) {
+      throw std::invalid_argument(std::string("Missing wheel velocity key for ROCKER_BOGIE: ") + k);
+    }
+    if (wheelSteeringAngles.find(k) == wheelSteeringAngles.end()) {
+      throw std::invalid_argument(std::string("Missing wheel steering key for ROCKER_BOGIE: ") + k);
+    }
+  }
+
+  // Calculate the robot body twist
+  const Eigen::Vector3d bodyTwist = this->rockerBogieForwardKinematics(wheelVelocities, wheelSteeringAngles);
+  // Convert to global frame
+  const double thetaOld = this->currentState.theta;
+  const double globalVX = bodyTwist(0) * std::cos(thetaOld) - bodyTwist(1) * std::sin(thetaOld);
+  const double globalVY = bodyTwist(0) * std::sin(thetaOld) + bodyTwist(1) * std::cos(thetaOld);
+  const double globalOmega = bodyTwist(2);
+
+  // Predict new state
+  const double xNew = this->currentState.x + globalVX * dt;
+  const double yNew = this->currentState.y + globalVY * dt;
+  const double thetaNew = this->currentState.theta + globalOmega * dt;
+  RobotState predictedState(xNew, yNew, thetaNew, globalVX, globalVY, globalOmega);
+
+  // Calculate the Jacobian (F)
+  const auto F = this->systemModel.getStateTransitionMatrix(dt, 0, 0, thetaOld, globalVX, globalVY);
+
+  // Update the state covariance with the process noise and state transition matrix
+  const auto P = this->covariance.stateCovariance;
+  const auto Q = this->covariance.processNoiseCovariance;
+  this->covariance.stateCovariance = F * P * F.transpose() + Q;
+
+  // Update the previous prediction timestamp
+  this->previousPredictionTimeStamp = timestamp;
+
+  // Return the predicted state
+  return predictedState;
 }
