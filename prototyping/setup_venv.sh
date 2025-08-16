a3a110a215ac8a98cb1328ecf78e001a595bcb24
Sharwin24 <sharwin24@gmail.com>
2025-08-15 21:46:12 -0500

    Moved setup and requirements to prototyping

--- prototyping/setup_venv.sh
+++ prototyping/setup_venv.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create a virtual environment if it doesn't exist
-if [ ! -d ".venv" ]; then
+if [ ! -d "../.venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv .venv
+    python3 -m venv ../.venv
 else
     echo "Virtual environment already exists."
 fi
 
 # Activate the virtual environment
-source .venv/bin/activate
+source ../.venv/bin/activate
 
 # Install required packages
 pip install -r requirements.txt
