7cea7b20e0516cb2f1df085fe65a67ee794eb716
Sharwin24 <sharwin24@gmail.com>
2025-09-01 17:05:51 -0500

    Changed board to Teensy 4.1 and outlined commands

--- RoverMotorControl/src/main.cpp
+++ RoverMotorControl/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/**
+ * This board (Teensy) needs to perform the following tasks:
+ * 1. Control 6 BLDC motors (6 PWM pins, 6 GPIO direction pins)
+ * 2. Control 4 servo motors (4 PWM pins)
+ * 3. Read from 6 encoders (12 GPIO pins)
+ * 4. Communicate with Raspberry Pi 5 over SPI to send encoder data and receive motor commands
+ *
+ * RPI5 -> Teensy:
+ * - 6 motor speeds
+ * - 4 servo positions (steering angles)
+ * Teensy -> RPI5:
+ * - 6 encoder positions (for wheel odometry)
+ *
+ */
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
